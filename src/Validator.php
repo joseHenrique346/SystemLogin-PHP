6b96034cb976d5ff850a736f2247a77675a503d1
José Henrique Fernandes <zehenrique0306@gmail.com>
2025-09-22 22:44:12 -0300

    feat(User) cenários criados

--- src/Validator.php
+++ src/Validator.php
@@ -5,7 +5,7 @@ class Validator
     public static function validatePassword(string $password): bool 
     {
         if (strlen($password) < 8 || !preg_match('/\d/', $password) || !preg_match('/[A-Z]/', $password)) {
-            echo("Senha inválida<br>");
+            echo("Senha '{$password}' inválida<br>");
             return false;
         }
 
@@ -15,7 +15,7 @@ class Validator
     public static function validateEmail(string $email) : bool
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo("Email Inválido<br>");
+            echo("Email '{$email}' Inválido<br>");
             return false;
         }
 
@@ -25,7 +25,6 @@ class Validator
     public static function validateUser(int $id, string $name, string $email, string $password) : bool
     {
         if ($id <= 0 || strlen($name) <= 1 || !Validator::validateEmail($email) || !Validator::validatePassword($password)) {
-            echo("Usuário inválido<br>");
             return false;
         }
 
--- src/index.php
+++ src/index.php
@@ -5,11 +5,16 @@ require_once 'UserEmail.php';
 require_once 'UserPassword.php';
 
 $userManager = new UserManager;
+echo("<h3>Criação dos usuários<br></h3>");
 $userManager->generateSimulatedUsers();
 
-User::login($userManager, "Email", "Senha");
-User::login($userManager, "maria@email.com", "Senha123");
+echo("<h3>Login com usuário inexiste<br></h3>");
+echo(User::login($userManager, "Email", "Senha"));
 
-User::updatePassword($userManager, "maria@email.com", "Senha1234")
+echo("<h3>Login normal<br></h3>");
+echo(User::login($userManager, "maria@email.com", "Senha123"));
+
+echo("<h3>Atualizar senha<br></h3>");
+echo(User::updatePassword($userManager, "maria@email.com", "Senha1234"));
 
 ?>
